999883b2a6bc3f65ae07cd68a1bac26ecd5638a7
ErenPamukIUE <erenpamuk7@gmail.com>
2025-11-30 02:53:31 +0300

    commit 3

--- src/com_ern_project/graphics/Screen.java
+++ src/com_ern_project/graphics/Screen.java
@@ -18,9 +18,11 @@ private Render test;
         for(int i =0 ; i<width*height ; i++) {
             pixels[i] = 0;
         }
-        for(int i = 0 ; i<100 ; i++) {
-            int anim = (int) (  (Math.sin((System.currentTimeMillis() + i) % 2000.0 / 2000 * Math.PI *2)) *200  );
-            int anim2 = (int) (  (Math.cos((System.currentTimeMillis() + i) % 2000.0 / 2000 * Math.PI *2)) *200  );
+
+
+        for(int i = 0 ; i<10 ; i++) {
+            int anim = (int) (  (Math.sin((System.currentTimeMillis() + i*3) % 2000.0 / 2000 * Math.PI *2)) *200  );
+            int anim2 = (int) (  (Math.cos((System.currentTimeMillis() + i*3) % 2000.0 / 2000 * Math.PI *2)) *200  );
             draw(test,(width - 256 ) / 2 + anim, (height - 256) /2 - anim2);
 
         }
